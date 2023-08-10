127fb8fba9cb6a9ea55140cd88ffc88ab80b8a55
pacochow <paco.ckchow@gmail.com>
2023-08-10 23:11:27 +0100

    Added more physics and floor

--- simulate.py
+++ simulate.py
@@ -1,11 +1,15 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
-
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
-    time.sleep(1/10000)
+    time.sleep(1/500)
     print(i)
 
 p.disconnect()
