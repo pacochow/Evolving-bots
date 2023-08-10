e99ddd29431ad12d724ddbd4c31e293f6e9e3661
pacochow <paco.ckchow@gmail.com>
2023-08-10 23:27:39 +0100

    Added more boxes

--- generate.py
+++ generate.py
@@ -1,8 +1,17 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 x = 0
 y = 0
-z = 1.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[1,2,3])
+z = -0.5
+
+a = 1
+b = 1
+c = 1
+for i in range (10):
+    z += 1
+    a *= 0.9
+    b *= 0.9
+    c *= 0.9
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[a,b,c])
 pyrosim.End()
