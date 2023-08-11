e1f80434643fa0b56b8f66a34f4c8c043f762015
pacochow <paco.ckchow@gmail.com>
2023-08-11 17:34:21 +0100

    Added motors

--- analyze.py
+++ analyze.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
 frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
-plt.plot(backLegSensorValues, label = "Back leg")
-plt.plot(frontLegSensorValues, label = "Front leg")
-plt.legend()
+targetAngles = np.load("data/targetAngles.npy")
+plt.plot(targetAngles)
+# plt.plot(backLegSensorValues, label = "Back leg")
+# plt.plot(frontLegSensorValues, label = "Front leg")
+# plt.legend()
 plt.show()
--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
 import numpy as np
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -12,7 +13,14 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-iterations = 100
+iterations = 1000
+BackLeg_amplitude = np.pi/4
+BackLeg_frequency = 0.05
+BackLeg_phaseOffset = 0
+
+FrontLeg_amplitude = np.pi/4
+FrontLeg_frequency = 0.05
+FrontLeg_phaseOffset = np.pi/4
 
 backLegSensorValues = np.zeros(iterations)
 frontLegSensorValues = np.zeros(iterations)
@@ -21,8 +29,31 @@ for i in range(iterations):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    time.sleep(1/50)
+    pyrosim.Set_Motor_For_Joint(
+
+        bodyIndex = robotId,
+
+        jointName = "Torso_BackLeg",
+
+        controlMode = p.POSITION_CONTROL,
+
+        targetPosition = BackLeg_amplitude*np.sin(BackLeg_frequency*i + BackLeg_phaseOffset),
+
+        maxForce = 50)
+    pyrosim.Set_Motor_For_Joint(
+
+        bodyIndex = robotId,
+
+        jointName = "Torso_FrontLeg",
+
+        controlMode = p.POSITION_CONTROL,
+
+        targetPosition = FrontLeg_amplitude*np.sin(FrontLeg_frequency*i + FrontLeg_phaseOffset),
+
+        maxForce = 50)
+    time.sleep(1/2400)
 
 p.disconnect()
 np.save("data/backLegSensorValues.npy", backLegSensorValues)
 np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
+# np.save("data/targetAngles.npy", targetAngles)
