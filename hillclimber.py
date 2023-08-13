15428a49473e9857757e9646be189c88aa2ba42a
pacochow <paco.ckchow@gmail.com>
2023-08-13 20:23:10 +0100

    Hillclimber file

---
+++ hillclimber.py
@@ -0,0 +1,36 @@
+from solution import SOLUTION
+import constants as c
+import copy
+
+class HILL_CLIMBER:
+    def __init__(self):
+        self.parent = SOLUTION()
+        
+    
+    def Evolve(self):
+        self.parent.Evaluate('GUI')
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
+            
+        self.Show_Best()
+        
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        print(self.parent.fitness, self.child.fitness)
+        self.Select()
+        
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+    def Mutate(self):
+        self.child.Mutate()
+
+    
+    def Select(self):
+        if self.parent.fitness > self.child.fitness:
+            self.parent=self.child
+            
+    def Show_Best(self):
+        self.parent.Evaluate("GUI")
